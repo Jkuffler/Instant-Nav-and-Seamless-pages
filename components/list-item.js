785d22a1d1acac8aad21981ffcb93e932ce8eb7d
Jason <jkuffler@catalyte.io>
2022-07-02 15:24:48 -0400

    speculation rules and prerender button added

--- components/list-item.js
+++ components/list-item.js
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+import PrerenderButton from './prerender'
 import Link from 'next/link'
 import Icon from './icon'
 // Codelab: Add a PrerenderButton component.
---
+++ components/prerender-button.js
@@ -0,0 +1,25 @@
+import {useContext} from 'react'
+import ResourceContext from './resource-context'
+
+/*Resource context: used to manage global states 
+This PrerenderButton component is meant to update the prerenderURL param on button click*/
+export default function PrerenderButton() {
+  const {dispatch} = useContext(ResourceContext)
+  const handleClick = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    const parent = e.target.closest('a')
+    if(!parent) {
+      return
+    }
+    const href = parent.getAttribute('href')
+    dispatch( {type: 'update', prerenderURL: href} )
+  }
+
+  return (
+    <button className='ml-auto bg-gray-200 hover:bg-gray-300 px-4 rounded' onClick={handleOnClick}>
+      Prerender
+    </button>
+  )
+
+}
